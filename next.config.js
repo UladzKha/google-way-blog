c9762805a417a6716aff1373a12cc2f58b1f10bc
Uladzimir <raer.camp@gmail.com>
2020-11-04 09:27:14 +0100

    next.config.js

--- next.config.js
+++ next.config.js
@@ -1,4 +1,6 @@
-export const env = {
-    MONGODB_URI: 'mongodb+srv://uladzik:C2KfbVayWbd9roCt@cluster0.p91jq.mongodb.net/uladz?retryWrites=true&w=majority',
-    MONGODB_DB: 'uladz'
-};
+module.exports = {
+    env: {
+        MONGODB_URI: 'mongodb+srv://uladzik:C2KfbVayWbd9roCt@cluster0.p91jq.mongodb.net/uladz?retryWrites=true&w=majority',
+        MONGODB_DB: 'uladz'
+    }
+}
