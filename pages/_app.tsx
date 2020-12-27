20fa514792d49cd1ca31e0356896e68996f0e1c3
Uladzimir Khadakouski <uladzimir.khadakouski@intive.com>
2020-12-27 16:59:52 +0100

    removed redundant files

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,11 +1,10 @@
 import { AppProps } from "next/app";
-import "../styles/globals.css";
 import "bootstrap/dist/css/bootstrap.min.css";
 import Header from "../components/header/Header";
 
 import "../components/header/header.scss";
 import "../sass/about.scss";
-import "../sass/blog.scss"
+import "../sass/blog.scss";
 
 function App({ Component, pageProps }: AppProps) {
   return (
